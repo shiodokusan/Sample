8a9bb95338da4da07ce7dae805a1f71cf92f9b6f
hashimoto yuko <hashimoto@hashimoto-no-MacBook-Pro.local>
2017-07-05 19:18:19 +0900

    編集橋本

--- FirstGithub.java
+++ FirstGithub.java
@@ -2,5 +2,6 @@ public class FirstGithub {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("ああああ");
+        System.out.println("iiiiii");
     }
 }
